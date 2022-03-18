76f5ea73bda00178ad5f4d88f8cb010db01ceb9e
glihm <dev@glihm.net>
2022-03-18 13:56:00 -0600

    add build scripts

--- native-aot/build.bat
+++ native-aot/build.bat
@@ -5,19 +5,33 @@ IF "%1" == "" (
    echo No C++ call
 )
 
-echo "%1"
+:: echo "%1"
 
 :: clean
 IF EXIST build RMDIR /S /Q build
 
 IF EXIST ..\libsharp\bin RMDIR /S /Q ..\libsharp\bin
+
 dotnet clean ../libsharp/LibSharp.csproj
 
-:: publish the C# library with AOT options thanks to the import package ILCompiler.
+:: publish the C# library with AOT options thanks to the imported package ILCompiler.
 IF "%1" == "cppcall" (
-dotnet publish --nologo -r win-x64 -c Release /p:DefineConstants=CALLCPP /p:SelfContained=true /p:NativeLib=Shared -o build ../libsharp/LibSharp.csproj
+dotnet publish --nologo^
+       	       -r win-x64^
+	       -c Release^
+	       /p:DefineConstants=CALLCPP^
+	       /p:SelfContained=true^
+	       /p:NativeLib=Shared^
+	       -o build^
+	       ../libsharp/LibSharp.csproj
 ) ELSE (
-dotnet publish --nologo -r win-x64 -c Release /p:SelfContained=true /p:NativeLib=Shared -o build ../libsharp/LibSharp.csproj
+dotnet publish --nologo^
+       	       -r win-x64^
+	       -c Release^
+	       /p:SelfContained=true^
+	       /p:NativeLib=Shared^
+	       -o build^
+	       ../libsharp/LibSharp.csproj
 )
 
 :: ------> ONLY IF NativeCpp is called into LibSharp <--------
@@ -30,7 +44,7 @@ IF "%1" == "cppcall" (
   :: So in this case, the main.exe... and not the LibSharp.dll.
   :: The build system has the responsability to organize those files.
   :: (all dlls in the same directory, etc...)
-  :: It will works because in the C# DllImport into libsharp/WrapCppLib.cs,
+  :: It will work because in the C# DllImport into libsharp/NativeCpp.cs,
   :: when published in release, the dll is referenced into build/.
    Copy ..\libnative\build\sum.dll build\sum.dll
 )
@@ -38,4 +52,4 @@ IF "%1" == "cppcall" (
 :: Build the main.
 cl.exe main.c -D WINDOWS
 :: then execute the main as follow
-:: > main.exe sharp-aot-build\LibSharp.dll
+:: > main.exe build\LibSharp.dll
